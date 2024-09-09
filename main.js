8f932d0503e7e776c7702224454d9f3d8f3a950b
johntrehearn <johntrehearnbc@gmail.com>
2024-09-09 13:11:08 +0300

    js WIP

---
+++ main.js
@@ -0,0 +1,13 @@
+const pokemonDisplay = document.querySelector('.pokeDisplay');
+const genSizeInfo = document.getElementById('.genSizeInfo');
+const searchBar = document.getElementById('.searchBar');
+const genButton = document.querySelectorAll('.btn');
+
+let pokeData = [];
+let newGen = '';
+const generations = [];
+
+const fetchData = async () => {
+
+    
+};
